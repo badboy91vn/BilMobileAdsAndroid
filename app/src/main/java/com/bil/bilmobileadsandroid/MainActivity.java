602a1136d079dd5443879c4a06027a64d2c891a7
badboy91vn <bad.boy91vn@yahoo.com>
2020-06-29 17:25:39 +0700

    v1.0.1

--- app/src/main/java/com/bil/bilmobileadsandroid/MainActivity.java
+++ app/src/main/java/com/bil/bilmobileadsandroid/MainActivity.java
@@ -3,12 +3,112 @@ package com.bil.bilmobileadsandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.bil.bilmobileads.ADBanner;
+import com.bil.bilmobileads.ADInterstitial;
+import com.bil.bilmobileads.ADRewarded;
+import com.bil.bilmobileads.PBMobileAds;
+import com.bil.bilmobileads.entity.BannerSize;
+import com.bil.bilmobileads.interfaces.AdDelegate;
 
 public class MainActivity extends AppCompatActivity {
+
+    ADBanner adBanner;
+    ADInterstitial adInterstitial;
+    ADRewarded adRewarded;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PBMobileAds.getInstance().initialize(getApplicationContext(), "21258", true);
+
+        FrameLayout bannerView = findViewById(R.id.bannerView);
+        // init banner
+        this.adBanner = new ADBanner(bannerView, "32588");
+        this.adBanner.setAdSize(BannerSize.Banner300x250);
+        this.adBanner.setAutoRefreshMillis(30000);
+        this.adBanner.load();
+        this.adBanner.setListener(new AdDelegate() {
+            @Override
+            public void onAdLoaded(String data) {
+                PBMobileAds.getInstance().log(data);
+            }
+
+            @Override
+            public void onAdImpression(String data) {
+                PBMobileAds.getInstance().log(data);
+            }
+
+            @Override
+            public void onAdClosed(String data) {
+                PBMobileAds.getInstance().log(data);
+            }
+
+            @Override
+            public void onAdFailedToLoad(String err) {
+                PBMobileAds.getInstance().log(err);
+            }
+
+            @Override
+            public void onAdLeftApplication(String data) {
+                PBMobileAds.getInstance().log(data);
+            }
+        });
+
+////         init full
+//        this.adInterstitial = new ADInterstitial("32593");
+//        this.adInterstitial.preLoad();
+//        this.adInterstitial.setListener(new AdDelegate() {
+//            @Override
+//            public void onAdLoaded(String data) {
+//                PBMobileAds.getInstance().log(data);
+//            }
+//
+//            @Override
+//            public void onAdImpression(String data) {
+//                PBMobileAds.getInstance().log(data);
+//            }
+//
+//            @Override
+//            public void onAdClosed(String data) {
+//                PBMobileAds.getInstance().log(data);
+//            }
+//
+//            @Override
+//            public void onAdFailedToLoad(String err) {
+//                PBMobileAds.getInstance().log(err);
+//            }
+//
+//            @Override
+//            public void onAdLeftApplication(String data) {
+//                PBMobileAds.getInstance().log(data);
+//            }
+//        });
+
+//        this.adRewarded = new ADRewarded(this, "32594");
+//        this.adRewarded.setListener(new AdRewardedDelegate() {
+//            @Override
+//            public void onRewardedAdOpened(String data) {
+//                PBMobileAds.getInstance().log(data);
+//            }
+//
+//            @Override
+//            public void onRewardedAdClosed(String data) {
+//                PBMobileAds.getInstance().log(data);
+//            }
+//
+//            @Override
+//            public void onUserEarnedReward(String data) {
+//                PBMobileAds.getInstance().log(data);
+//            }
+//
+//            @Override
+//            public void onRewardedAdFailedToShow(String data) {
+//                PBMobileAds.getInstance().log(data);
+//            }
+//        });
     }
 }
