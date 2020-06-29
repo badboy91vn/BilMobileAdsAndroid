d50fa1d6d382a484cc510acb54dd099cc083b8bd
badboy91vn <bad.boy91vn@yahoo.com>
2020-06-29 10:39:15 +0700

    V1.0.0

---
+++ bilmobileads/src/main/java/com/bil/bilmobileads/ADRewarded.java
@@ -0,0 +1,258 @@
+package com.bil.bilmobileads;
+
+import android.app.Activity;
+
+import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
+import com.google.android.gms.ads.rewarded.RewardItem;
+import com.google.android.gms.ads.rewarded.RewardedAd;
+import com.google.android.gms.ads.rewarded.RewardedAdCallback;
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
+import com.bil.bilmobileads.entity.ADFormat;
+import com.bil.bilmobileads.entity.AdInfor;
+import com.bil.bilmobileads.entity.AdUnitObj;
+import com.bil.bilmobileads.entity.TimerRecall;
+import com.bil.bilmobileads.interfaces.AdRewardedDelegate;
+import com.bil.bilmobileads.interfaces.TimerCompleteListener;
+
+import org.prebid.mobile.AdUnit;
+import org.prebid.mobile.OnCompleteListener;
+import org.prebid.mobile.ResultCode;
+import org.prebid.mobile.RewardedVideoAdUnit;
+import org.prebid.mobile.Signals;
+import org.prebid.mobile.VideoBaseAdUnit;
+
+import java.util.Arrays;
+
+import androidx.annotation.NonNull;
+
+public class ADRewarded {
+
+    // MARK: - View
+    private Activity activityAd;
+    private AdRewardedDelegate adDelegate;
+
+    // MARK: - AD
+    private PublisherAdRequest amRequest;
+    private AdUnit adUnit;
+    private RewardedAd amRewarded;
+    // MARK: - AD info
+    private String placement;
+    private AdUnitObj adUnitObj;
+
+    // MARK: Properties
+    private ADFormat adFormatDefault;
+    private boolean isLoadAfterPreload = false; // Check user gọi load() chưa có AD -> preload và show AD luôn
+    private boolean isFetchingAD = false; // Check có phải đang lấy AD ko
+    private boolean isRecallingPreload = false; // Check đang đợi gọi lại preload
+    private TimerRecall timerRecall; // Recall load func AD
+
+    public ADRewarded(Activity activity, String placement) {
+        if (activity == null || placement == null) {
+            PBMobileAds.getInstance().log("Activity and Placement is not nullable");
+            throw new NullPointerException();
+        }
+        PBMobileAds.getInstance().log("AD Interstitial Init: " + placement);
+
+        this.activityAd = activity;
+        this.placement = placement;
+        this.amRequest = new PublisherAdRequest.Builder().build();
+
+        this.timerRecall = new TimerRecall(Constants.RECALL_CONFIGID_SERVER, 1000, new TimerCompleteListener() {
+            @Override
+            public void doWork() {
+                isRecallingPreload = false;
+                preLoad();
+            }
+        });
+
+        PBMobileAds.getInstance().listADRewarded.add(this);
+    }
+
+    // MARK: - Preload + Load
+    void deplayCallPreload() {
+        this.isRecallingPreload = true;
+        this.timerRecall.start();
+    }
+
+    AdInfor getAdInfor(boolean isVideo) {
+        for (AdInfor infor : this.adUnitObj.adInfor) {
+            if (infor.isVideo == isVideo) {
+                return infor;
+            }
+        }
+        return null;
+    }
+
+    void handerResult(ResultCode resultCode) {
+        if (resultCode == ResultCode.SUCCESS) {
+            this.amRewarded.loadAd(this.amRequest, new RewardedAdLoadCallback() {
+                @Override
+                public void onRewardedAdLoaded() {
+                    PBMobileAds.getInstance().log("RewarededVideo load SUCC");
+                    isFetchingAD = false;
+
+                    if (isLoadAfterPreload) {
+                        isLoadAfterPreload = false;
+                        if (isReady()) show();
+                    }
+                }
+
+                @Override
+                public void onRewardedAdFailedToLoad(int errorCode) {
+                    PBMobileAds.getInstance().log("RewardedVideo load failed: " + errorCode);
+                    isFetchingAD = false;
+
+                    if (!isLoadAfterPreload) deplayCallPreload();
+                }
+            });
+        } else {
+            if (resultCode == ResultCode.NO_BIDS) {
+                this.adFormatDefault = this.adFormatDefault.equals(ADFormat.HTML) ? ADFormat.VAST : ADFormat.HTML;
+                // Ko gọi lại preload nếu user gọi load() đầu tiên
+                if (!this.isLoadAfterPreload) this.deplayCallPreload();
+            } else if (resultCode == ResultCode.TIMEOUT) {
+                // Ko gọi lại preload nếu user gọi load() đầu tiên
+                if (!this.isLoadAfterPreload) this.deplayCallPreload();
+            }
+
+            this.isFetchingAD = false;
+        }
+    }
+
+    public boolean preLoad() {
+        PBMobileAds.getInstance().log("PBMobileAds: " + PBMobileAds.getInstance().isInitialize() + " | isReady: " + this.isReady() + " |  isFetchingAD: " + this.isFetchingAD + " |  isRecallingPreload: " + this.isRecallingPreload);
+        if (PBMobileAds.getInstance().isInitialize() == false || this.isReady() == true
+                || this.isFetchingAD == true || this.isRecallingPreload == true) {
+            return false;
+        }
+        PBMobileAds.getInstance().log("Preload Rewareded AD: " + this.placement);
+
+        // Get Data Config
+        if (this.adUnitObj == null) {
+            AdUnitObj adUnitO = PBMobileAds.getInstance().getAdUnitObj(this.placement);
+            if (adUnitO == null) {
+                PBMobileAds.getInstance().log("Placement is not exist");
+                return false;
+            }
+            this.adUnitObj = adUnitO;
+        }
+
+        // Check Active
+        if (!this.adUnitObj.isActive || this.adUnitObj.adInfor.size() <= 0) {
+            PBMobileAds.getInstance().log("Ad is not active or not exist");
+            return false;
+        }
+
+        // Check and set default
+        this.adFormatDefault = this.adFormatDefault == null ? this.adUnitObj.defaultType : this.adFormatDefault;
+        // set adFormat theo loại duy nhất có
+        if (adUnitObj.adInfor.size() < 2) {
+            this.adFormatDefault = this.adUnitObj.adInfor.get(0).isVideo ? ADFormat.VAST : ADFormat.HTML;
+        }
+
+        AdInfor adInfor;
+
+        AdInfor info = this.getAdInfor(true);
+        if (info == null) {
+            PBMobileAds.getInstance().log("AdInfor is not exist");
+            return false;
+        }
+        adInfor = info;
+
+        PBMobileAds.getInstance().setupPBS(adInfor.host);
+        PBMobileAds.getInstance().log("[Full Video] - configId: " + adInfor.configId + " | adUnitID: " + adInfor.adUnitID);
+
+        VideoBaseAdUnit.Parameters parameters = new VideoBaseAdUnit.Parameters();
+        parameters.setMimes(Arrays.asList("video/mp4"));
+        parameters.setProtocols(Arrays.asList(Signals.Protocols.VAST_2_0));
+        parameters.setPlaybackMethod(Arrays.asList(Signals.PlaybackMethod.AutoPlaySoundOn));
+        parameters.setPlacement(Signals.Placement.InBanner);
+
+        RewardedVideoAdUnit rAdUnit = new RewardedVideoAdUnit(adInfor.configId);
+        rAdUnit.setParameters(parameters);
+        this.adUnit = rAdUnit;
+
+        this.amRewarded = new RewardedAd(PBMobileAds.getInstance().getContextApp(), adInfor.adUnitID);
+
+        this.isFetchingAD = true;
+        this.adUnit.fetchDemand(this.amRequest, new OnCompleteListener() {
+            @Override
+            public void onComplete(ResultCode resultCode) {
+                PBMobileAds.getInstance().log("Prebid demand fetch for DFP " + resultCode.name());
+                handerResult(resultCode);
+            }
+        });
+
+        return true;
+    }
+
+    public void load() {
+        if (this.isReady()) {
+            this.show();
+        } else {
+            PBMobileAds.getInstance().log("Don't have AD");
+            this.isLoadAfterPreload = true;
+            this.preLoad();
+        }
+    }
+
+    private void show() {
+        this.amRewarded.show(this.activityAd, new RewardedAdCallback() {
+            @Override
+            public void onRewardedAdOpened() {
+                if (adDelegate == null) return;
+                adDelegate.onRewardedAdOpened("onRewardedAdOpened");
+            }
+
+            @Override
+            public void onRewardedAdClosed() {
+                if (adDelegate == null) return;
+                adDelegate.onRewardedAdOpened("onRewardedAdClosed");
+            }
+
+            @Override
+            public void onUserEarnedReward(@NonNull RewardItem reward) {
+                if (adDelegate == null) return;
+                adDelegate.onRewardedAdOpened("onUserEarnedReward");
+            }
+
+            @Override
+            public void onRewardedAdFailedToShow(int errorCode) {
+                if (adDelegate == null) return;
+
+                switch (errorCode) {
+                    case ERROR_CODE_INTERNAL_ERROR:
+                        adDelegate.onRewardedAdOpened("INTERNAL_ERROR");
+                        break;
+                    case ERROR_CODE_AD_REUSED:
+                        adDelegate.onRewardedAdOpened("AD_REUSED");
+                        break;
+                    case ERROR_CODE_NOT_READY:
+                        adDelegate.onRewardedAdOpened("NOT_READY");
+                        break;
+                    case ERROR_CODE_APP_NOT_FOREGROUND:
+                        adDelegate.onRewardedAdOpened("APP_NOT_FOREGROUND");
+                        break;
+                }
+            }
+        });
+    }
+
+    public void destroy() {
+        PBMobileAds.getInstance().log("Destroy Placement: " + this.placement);
+        this.isLoadAfterPreload = false;
+        this.adUnit.stopAutoRefresh();
+    }
+
+    public boolean isReady() {
+        if (this.amRewarded == null) return false;
+
+        PBMobileAds.getInstance().log("AD IsReady: " + this.amRewarded.isLoaded());
+
+        return this.amRewarded.isLoaded();
+    }
+
+    public void setListener(AdRewardedDelegate adDelegate) {
+        this.adDelegate = adDelegate;
+    }
+}
---
+++ bilmobileads/src/main/java/com/bil/bilmobileads/PBMobileAds.java
@@ -0,0 +1,221 @@
+
+package com.bil.bilmobileads;
+
+import android.content.Context;
+import android.util.Log;
+import android.webkit.WebView;
+
+import com.bil.bilmobileads.entity.ADFormat;
+import com.bil.bilmobileads.entity.AdInfor;
+import com.bil.bilmobileads.entity.AdUnitObj;
+import com.bil.bilmobileads.entity.HostCustom;
+import com.bil.bilmobileads.entity.TimerRecall;
+import com.bil.bilmobileads.interfaces.ResultCallback;
+import com.bil.bilmobileads.interfaces.TimerCompleteListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.prebid.mobile.Host;
+import org.prebid.mobile.PrebidMobile;
+import org.prebid.mobile.TargetingParams;
+
+import java.util.ArrayList;
+
+public class PBMobileAds {
+
+    private static final PBMobileAds instance = new PBMobileAds();
+
+    // MARK: Context app
+    private Context contextApp;
+
+    // MARK: List Config
+    ArrayList<AdUnitObj> listAdUnitObj = new ArrayList<AdUnitObj>();
+
+    // MARK: List AD
+    ArrayList<ADBanner> listADBanner = new ArrayList<ADBanner>();
+    ArrayList<ADInterstitial> listADIntersititial = new ArrayList<ADInterstitial>();
+    ArrayList<ADRewarded> listADRewarded = new ArrayList<ADRewarded>();
+
+    // MARK: api
+    boolean isLog = true;
+    boolean isConfigSucc = false;
+    TimerRecall timerRecall;
+
+    private String pbServerEndPoint = "";
+    private String configId;
+
+
+    private PBMobileAds() {
+        this.log("PBMobileAds Init");
+
+        timerRecall = new TimerRecall(Constants.RECALL_CONFIGID_SERVER, 1000);
+        timerRecall.setListener(new TimerCompleteListener() {
+            @Override
+            public void doWork() {
+                getADConfig();
+            }
+        });
+    }
+
+    public static PBMobileAds getInstance() {
+        return instance;
+    }
+
+    // MARK: - initialize
+    public void initialize(Context context, String configId, boolean testMode) {
+//      if !isLog { PrebidMobile.logLevel = .error }
+
+        this.contextApp = context;
+
+        this.configId = configId;
+
+        //Declare in init to the user agent could be passed in first call
+        PrebidMobile.setShareGeoLocation(true);
+        PrebidMobile.setApplicationContext(context);
+        WebView obj = new WebView(context);
+        obj.clearCache(true);
+
+        // Setup Test Mode
+        if (testMode == true) {
+//            GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers =  [ (kGADSimulatorID as! String), "cc7ca766f86b43ab6cdc92bed424069b"];
+        }
+
+        getADConfig();
+    }
+
+    public Context getContextApp() {
+        return this.contextApp;
+    }
+
+    // MARK: - Call API AD
+    private void getADConfig() {
+        this.log("Start Request Config");
+
+        HttpApi httpApi = new HttpApi<JSONObject>(Constants.GET_DATA_CONFIG
+                + "?appId=" + this.configId, new ResultCallback<JSONObject, Exception>() {
+            @Override
+            public void success(JSONObject dataJSON) {
+//                log("Data: " + dataJSON.toString());
+
+                timerRecall.cancel();
+                isConfigSucc = true;
+                try {
+                    pbServerEndPoint = dataJSON.getString("pbServerEndPoint");
+                    JSONArray adunitArray = dataJSON.getJSONArray("adunit");
+
+                    // Set all ad type config
+                    for (int i = 0; i < adunitArray.length(); i++) {
+                        JSONObject adunitJsonObj = adunitArray.getJSONObject(i);
+
+                        String placement = adunitJsonObj.getString("placement");
+                        String type = adunitJsonObj.getString("type");
+                        ADFormat defaultType = adunitJsonObj.getString("defaultType").equalsIgnoreCase(ADFormat.HTML.toString()) ? ADFormat.HTML : ADFormat.VAST;
+                        boolean isActive = adunitJsonObj.getBoolean("isActive");
+
+                        // Create AdInfor
+                        ArrayList<AdInfor> adInforList = new ArrayList<AdInfor>();
+                        JSONArray adInforArray = adunitJsonObj.getJSONArray("adInfor");
+                        for (int j = 0; j < adInforArray.length(); j++) {
+                            JSONObject adInforObj = adInforArray.getJSONObject(j);
+                            boolean isVideo = adInforObj.getBoolean("isVideo");
+                            String configId = adInforObj.getString("configId");
+                            String adUnitID = adInforObj.getString("adUnitID");
+                            // Create Host
+                            JSONObject hostObj = adInforObj.getJSONObject("host");
+                            String pbHost = hostObj.getString("pbHost");
+                            String pbAccountId = hostObj.getString("pbAccountId");
+                            String storedAuctionResponse = hostObj.getString("storedAuctionResponse");
+                            HostCustom hostCustom = new HostCustom(pbHost, pbAccountId, storedAuctionResponse);
+
+                            AdInfor adInfor = new AdInfor(isVideo, hostCustom, configId, adUnitID);
+                            adInforList.add(adInfor);
+                        }
+
+                        AdUnitObj adUnitObj = new AdUnitObj(placement, type, defaultType, isActive, adInforList);
+                        listAdUnitObj.add(adUnitObj);
+                    }
+
+                    // Call all ad init before
+                    log("Banner Count: " + listADBanner.size());
+                    for (ADBanner ad : listADBanner) {
+                        ad.load();
+                    }
+                    log("Full Count: " + listADIntersititial.size());
+                    for (ADInterstitial ad : listADIntersititial) {
+                        ad.preLoad();
+                    }
+                    log("Rewarded Count: " + listADRewarded.size());
+                    for (ADRewarded ad : listADRewarded) {
+                        ad.preLoad();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    PBMobileAds.getInstance().log(e.getLocalizedMessage());
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                    PBMobileAds.getInstance().log(e.getLocalizedMessage());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    PBMobileAds.getInstance().log(e.getLocalizedMessage());
+                }
+            }
+
+            @Override
+            public void failure(Exception error) {
+                log("Err: " + error.getLocalizedMessage());
+
+                isConfigSucc = false;
+                timerRecall.start();
+            }
+        });
+        httpApi.execute();
+    }
+
+    // MARK: - Get Data Config
+    AdUnitObj getAdUnitObj(String placement) {
+        for (AdUnitObj config : this.listAdUnitObj) {
+            if (config.placement.equalsIgnoreCase(placement)) {
+                return config;
+            }
+        }
+        return null;
+    }
+
+    // MARK: Setup PBS
+    public void setupPBS(HostCustom hostCus) {
+        this.log("Host: " + hostCus.pbHost + " | AccountId: " + hostCus.pbAccountId + " | storedAuctionResponse: " + hostCus.storedAuctionResponse);
+        if (hostCus.pbHost.equalsIgnoreCase("Appnexus")) {
+            PrebidMobile.setPrebidServerHost(Host.APPNEXUS);
+        } else if (hostCus.pbHost.equalsIgnoreCase("Rubicon")) {
+            PrebidMobile.setPrebidServerHost(Host.RUBICON);
+        } else if (hostCus.pbHost.equalsIgnoreCase("Custom")) {
+            this.log("Custom URL: " + this.pbServerEndPoint);
+            Host.CUSTOM.setHostUrl(this.pbServerEndPoint);
+            PrebidMobile.setPrebidServerHost(Host.CUSTOM);
+        }
+
+        PrebidMobile.setPrebidServerAccountId(hostCus.pbAccountId);
+        PrebidMobile.setStoredAuctionResponse(hostCus.storedAuctionResponse);
+    }
+
+    public boolean isInitialize() {
+        return isConfigSucc;
+    }
+
+    public void enableCOPPA() {
+        TargetingParams.setSubjectToGDPR(true);
+    }
+
+    public void disableCOPPA() {
+        TargetingParams.setSubjectToGDPR(false);
+    }
+
+    public boolean log(String object) {
+        if (!isLog) {
+            return false;
+        }
+        Log.d("PBMobileAds", object);
+        return false;
+    }
+}
