65f0df27a7792bfce6a26252c4d75b56b94847ed
badboy91vn <bad.boy91vn@yahoo.com>
2020-06-30 17:40:56 +0700

    v1.0.7

--- app/src/main/java/com/bil/bilmobileadsandroid/MainActivity.java
+++ app/src/main/java/com/bil/bilmobileadsandroid/MainActivity.java
@@ -29,65 +29,17 @@ public class MainActivity extends AppCompatActivity {
         PBMobileAds.getInstance().initialize(getApplicationContext());
 
         FrameLayout bannerView = findViewById(R.id.bannerView);
-        // init banner
-//        this.adBanner = new ADBanner(bannerView, "ba87c06b-8fdf-4f19-9c63-4777e8957295");
-//        this.adBanner.setAdSize(BannerSize.Banner300x250);
-//        this.adBanner.setAutoRefreshMillis(30000);
-//        this.adBanner.setListener(new AdDelegate() {
-//            @Override
-//            public void onAdLoaded(String data) {
-//                PBMobileAds.getInstance().log(data);
-//            }
-//
-//            @Override
-//            public void onAdImpression(String data) {
-//                PBMobileAds.getInstance().log(data);
-//            }
-//
-//            @Override
-//            public void onAdClosed(String data) {
-//                PBMobileAds.getInstance().log(data);
-//            }
-//
-//            @Override
-//            public void onAdFailedToLoad(String err) {
-//                PBMobileAds.getInstance().log(err);
-//            }
-//
-//            @Override
-//            public void onAdLeftApplication(String data) {
-//                PBMobileAds.getInstance().log(data);
-//            }
-//        });
+        //  init banner  | ba87c06b-8fdf-4f19-9c63-4777e8957295
+        this.adBanner = new ADBanner(bannerView, "1001");
+        this.adBanner.setAdSize(BannerSize.Banner300x250);
+        this.adBanner.setAutoRefreshMillis(30000);
+        this.adBanner.load();
 
 //        this.adInterstitial = new ADInterstitial("1002");
 //        this.adInterstitial.preLoad();
-//        this.adInterstitial.setListener(new AdDelegate() {
-//            @Override
-//            public void onAdLoaded(String data) {
-//                PBMobileAds.getInstance().log(data);
-//            }
-//
-//            @Override
-//            public void onAdImpression(String data) {
-//                PBMobileAds.getInstance().log(data);
-//            }
-//
-//            @Override
-//            public void onAdClosed(String data) {
-//                PBMobileAds.getInstance().log(data);
-//            }
-//
-//            @Override
-//            public void onAdFailedToLoad(String err) {
-//                PBMobileAds.getInstance().log(err);
-//            }
-//
-//            @Override
-//            public void onAdLeftApplication(String data) {
-//                PBMobileAds.getInstance().log(data);
-//            }
-//        });
+
+        this.adRewarded = new ADRewarded(this, "1003");
+        this.adRewarded.preLoad();
 
         Button btnShowFull = (Button) findViewById(R.id.showFull);
         btnShowFull.setOnClickListener(new View.OnClickListener() {
@@ -99,27 +51,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        this.adRewarded = new ADRewarded(this, "1003");
-        this.adRewarded.setListener(new AdRewardedDelegate() {
-            @Override
-            public void onRewardedAdOpened(String data) {
-                PBMobileAds.getInstance().log(data);
-            }
-
-            @Override
-            public void onRewardedAdClosed(String data) {
-                PBMobileAds.getInstance().log(data);
-            }
-
-            @Override
-            public void onUserEarnedReward(String data) {
-                PBMobileAds.getInstance().log(data);
-            }
-
-            @Override
-            public void onRewardedAdFailedToShow(String data) {
-                PBMobileAds.getInstance().log(data);
-            }
-        });
     }
 }
--- bilmobileads/src/main/java/com/bil/bilmobileads/ADBanner.java
+++ bilmobileads/src/main/java/com/bil/bilmobileads/ADBanner.java
@@ -91,8 +91,6 @@ public class ADBanner {
                         PBMobileAds.getInstance().log(error.getLocalizedMessage());
                     }
                 });
-            } else {
-                this.load();
             }
         }
     }
